4f03d97aa9f216d60c93888db94aaa69ea9a62ec
samsj01 <samuel.suarez699@pascualbravo.edu.co>
2026-09-17 10:09:24 -0500

    Cambios jugador

--- PixelEscape/Jugador.cs
+++ PixelEscape/Jugador.cs
@@ -14,18 +14,19 @@ namespace PixelEscape
         int puntaje;
         int velocidad;
         int velY = 0; // nuevo
-        const int GRAVEDAD = -1; // nuevo
+        const int GRAVEDAD = 1; // nuevo
         const int FUERZASALTO = 3; // nuevo 
         const int SUELO = 0; // nuevo
         bool estaVivo;
 
-        public int X { get => x; }
-        public int Y { get => y; }
-        public int Vida { get => vida; set => vida = value; }
+        public int X { get => x; set => x = value; }
+        public int Y { get => y; set => y = value; }
+        public int Vida { get => vida; set => vida = value;}
         public int Velocidad { get => velocidad; }
 
         public bool EnElSuelo { get; private set; } = true;
         public int Puntaje { get => puntaje; set => puntaje = value; }
+        public int VelY { get => velY; set => velY = value; }
 
         public Jugador()
         {
@@ -37,140 +38,38 @@ namespace PixelEscape
             this.estaVivo = true;
         }
 
-        public void Moverse(ConsoleKey tecla, Moneda moneda1, Llave llave1,
-            CazadorPixel cazador, GuardianPixel guardian, Trampa trampita)
+        public bool MoverHorizontal(int pasos)
         {
-            switch (tecla)
-            {
-                case ConsoleKey.A:
-                    for (int i = 0; i < this.velocidad; i++)
-                    {
-                        this.x--;
-                        Console.Write($"{this.x},{this.y} ");
-                        RecogerObjetos(moneda1,llave1);
-                        PerderVida(cazador, guardian, trampita);
-                    }
-                    break;
-                case ConsoleKey.D:
-                    for (int i = 0; i < this.velocidad; i++)
-                    {
-                        this.x++;
-                        Console.Write($"{this.x},{this.y} ");
-                        RecogerObjetos(moneda1,llave1);
-                        PerderVida(cazador, guardian, trampita);
-                    }
-                    break;
-                case ConsoleKey.W:
-                    Saltar(tecla);
-                    break;
-                case ConsoleKey.Spacebar:
-                    Atacar();
-                    break;
-                default:
-                    break;
-            }
+            this.X += pasos * velocidad;
+            return true;
         }
 
-        public void Saltar(ConsoleKey tecla)
+        public void Saltar()
         {
             if (EnElSuelo)
             {
-                this.velY = FUERZASALTO;
+                this.Y += FUERZASALTO * VelY;
                 EnElSuelo = false;
-                Console.WriteLine($"{this.x},{this.y}");
-
             }
-        }
-        public void AplicarFisica()
+        } 
+
+        public void aterrizar()
         {
             if (!EnElSuelo)
             {
-                y += velY;
-                velY += GRAVEDAD;
-
-                if (Y <= SUELO)
-                {
-                    y = SUELO;
-                    velY = 0;
-                    EnElSuelo = true;
-                    Console.WriteLine($"(Aterrizaje) Posición: ({X}, {Y})");
-                }
-                else
-                {
-                    Console.WriteLine($"(En Aire) Posición: ({X}, {Y})");
-                }
+                this.Y -= this.VelY;
+                this.VelY -= GRAVEDAD;
             }
         }
-
-        public void Atacar()
+        public void RecibirDano(int dano)
         {
-            Console.WriteLine("ATAQUE");
-        }
-
-        public void RecogerObjetos(Moneda moneda1, Llave llave1)
-        {
-            
-            if (this.x == moneda1.X)
+            this.Vida -= dano;
+            if(this.Vida < 0)
             {
-                Console.WriteLine("\nRecogiste una moneda");
-                moneda1.Recogida = true;
-                moneda1.SerRecogida(this);
-            }
-            if (!llave1.Recogida)
-            {
-                if (llave1.X == this.X)
-                {
-                    Console.WriteLine("\nSe ha hagarrado la llave");
-                    llave1.Recoger(this);
-                }
-            }
-            
-        }
-
-        public void RecogerPW()
-        {
-
-        }
-
-        public void PerderVida(CazadorPixel cazador, 
-            GuardianPixel guardian, Trampa trampa)
-        {
-           if((cazador.PosicionX == this.X) && this.Vida >0)
-           {
-                this.Vida -= cazador.Atacar(this);
-                Console.WriteLine($"Te quedan {this.Vida} vidas");
-           }else
-            {
-                Muerte();
-            }
-
-            if ((trampa.PosicionX == this.X) && this.Vida > 0)
-            {
-                this.Vida -= trampa.CausarDano();
-                Console.WriteLine($"Te quedan {this.Vida} vidas");
-            }
-            else
-            {
-                Muerte();
-            }
-            if ((guardian.X == this.X) && this.Vida > 0)
-            {
-                this.Vida -= guardian.Atacar(this);
-                Console.WriteLine($"Te quedan {this.Vida} vidas");
-            }
-            else
-            {
-                Muerte();
+                this.Vida = 0;
             }
         }
-        public void Muerte()
-        {
-            if(this.Vida == 0)
-            {
-                Console.WriteLine("Estas Muerto");
-                this.estaVivo = false;
-            }
-            
-        }
+
+        
     }
 }
--- PixelEscape/Program.cs
+++ PixelEscape/Program.cs
@@ -11,39 +11,13 @@ namespace PixelEscape
     {
         static void Main(string[] args)
         {
-            Trampa trampita = new Trampa(17,0,1);
-            Power_up pw = new Power_up(3,0,10,"Escudo");
-            GuardianPixel enemigo1 = new GuardianPixel();
-            CazadorPixel cazador = new CazadorPixel(); 
-            Llave llave1 = new Llave(20,0);
             Jugador escapist = new Jugador();
-            Moneda moneda1 = new Moneda();
-            Console.WriteLine($"el jugador esta posicionado en {escapist.X},{escapist.Y}");
-            Console.WriteLine("Controles: A (Izquierda), D (Derecha), " +
-                "W (Saltar), ESC (Salir)\n");
 
-            while (true)
-            {
-                // Lectura de teclado no bloqueante para mantener la física corriendo
-                if (Console.KeyAvailable)
-                {
-                    ConsoleKeyInfo teclaInfo = Console.ReadKey(intercept: true);
+            Console.WriteLine($"x,y: {escapist.X},{escapist.Y}");
+            
+            escapist.MoverHorizontal(-5);
 
-                    if (teclaInfo.Key == ConsoleKey.Escape)
-                        break;
-
-                    escapist.Moverse(teclaInfo.Key, moneda1, llave1, 
-                        cazador,enemigo1,trampita);
-                }
-                escapist.AplicarFisica();
-                Thread.Sleep(150);
-            }
-            Console.Clear();
-            Console.WriteLine("GuardianPixel Patrullando");
-            while (true)
-            {
-                enemigo1.Patrullar();
-            }
+            Console.WriteLine($"x,y: {escapist.X},{escapist.Y}");
 
         }
     }
