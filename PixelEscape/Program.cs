3024c6fe42ef343dd062757fed915c945c0d9a37
samsj01 <samuel.suarez699@pascualbravo.edu.co>
2026-09-21 16:12:59 -0500

    Actua

--- PixelEscape/Program.cs
+++ PixelEscape/Program.cs
@@ -12,12 +12,21 @@ namespace PixelEscape
         static void Main(string[] args)
         {
             Jugador escapist = new Jugador();
-
+            
+            // posición Predefinida.
+            Console.WriteLine($"x,y: {escapist.X},{escapist.Y}");
+            //Mover 5 pasos a la izquierda.
+            escapist.MoverHorizontal(-5);
+            //nueva posición.
             Console.WriteLine($"x,y: {escapist.X},{escapist.Y}");
             
-            escapist.MoverHorizontal(-5);
+            //Se crea cazador
+            CazadorPixel cazador = new CazadorPixel();
+            Console.WriteLine($"Jugador tiene {escapist.Vida} vidas");
+            //Cazador hiere
+            escapist.RecibirDano(cazador.Atacar(escapist));
+            Console.WriteLine($"Jugador tiene {escapist.Vida} vidas");
 
-            Console.WriteLine($"x,y: {escapist.X},{escapist.Y}");
 
         }
     }
